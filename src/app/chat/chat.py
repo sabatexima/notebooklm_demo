01ae7da2035206fb99525fd64a6b798ef754be07
sabatexima <kinokosaibaiki@i.softbank.jp>
2025-07-11 21:16:59 +0900

    Refactor chat blueprint to use dedicated static and template folders.

--- src/app/chat/chat.py
+++ src/app/chat/chat.py
@@ -3,7 +3,10 @@ from flask import Blueprint,render_template,Flask, request, jsonify
 import app.chat.chatModel as chatModel
 
 # Blueprintを作成する
-chat = Blueprint("chat", __name__)
+chat = Blueprint("chat", __name__,
+                    template_folder='templates',
+                    static_folder='static',
+                    static_url_path='/chat/static')
 
 
 # ルートURLへのアクセスがあった場合にindex.htmlを返す
